d15cfd7df0d3d0ae79ee95b96c3acb7a19bd308a
Anziko Fred <anzikofred@gmail.com>
2025-01-17 12:59:37 +0300

    completed firebase setup on 17th Jan 2025

---
+++ functions/main.py
@@ -0,0 +1,6 @@
+# Welcome to Cloud Functions for Firebase for Python!
+# To get started, simply uncomment the below code or create your own.
+# Deploy with `firebase deploy`
+
+#import firebase_admin
+print("Hello World")
